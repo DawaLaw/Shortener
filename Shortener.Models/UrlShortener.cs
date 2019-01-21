0c647fce1fab573e0e7c821e1eddd75d6a9a3d09
Dawa Law <dawa.law@live.com>
2019-01-21 22:54:41 +0800

    Update Label

--- Shortener.Models/UrlShortener.cs
+++ Shortener.Models/UrlShortener.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shortener.Models
 {
     public class UrlShortener
     {
+        [Url]
+        [Display(Name = "Shortened URL")]
         public string ShortUrl { get; set; }
+
+        [Url]
+        [Required]
+        [Display(Name = "Long URL")]
         public string LongUrl { get; set; }
     }
 }
---
+++ Shortener.Tests/HomeControllerTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Shortener.Models;
+using Shortener.Web.Controllers;
+using System;
+using Xunit;
+
+namespace Shortener.Tests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexPostTest_PassTest()
+        {
+            // Arrange
+            var controller = new HomeController();
+            var shortener = new UrlShortener() { LongUrl = "http://facebook.com" };
+
+            // Action
+            var outcome = controller.Index(shortener);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(outcome);
+            Assert.Equal("https://localhost:53469/1234567", ((UrlShortener)viewResult.Model).ShortUrl);
+        }
+    }
+}
